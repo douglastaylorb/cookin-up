c2f6325b9cab5d27d498abb02228e475e95a5448
Douglas Taylor <78963489+douglastaylorb@users.noreply.github.com>
2024-11-07 14:30:05 -0300

    Final

--- src/components/SelecionarIngredientes.vue
+++ src/components/SelecionarIngredientes.vue
@@ -28,6 +28,7 @@ import CardCategoria from './CardCategoria.vue';
 import BotaoPrincipal from './BotaoPrincipal.vue';
 
 export default {
+  name: 'SelecionarIngredientes',
   data() {
     return {
     categorias: [] as ICategoria[],
